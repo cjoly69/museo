1b37113e278fefdf82d8d4d3cb0a9acf96e76b65
cjoly69 <cjoly.webdesign@gmail.com>
2016-11-26 14:50:55 +0100

    affichage blog et vue articles dans l'index

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+include "articles.php";
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Muséo</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,300,700">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-2">
+                    <a href="index.html">Muséo</a>
+                </div>
+                <div class="col-sm-10">
+                    <nav>
+                        <ul>
+                            <li><a href="index.html">Accueil</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                            <li><a href="connexion.html">Connexion</a></li>
+                            <li><a href="inscription.html">Inscription</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container article">
+        <div class="row">
+            <form method="post" action="">
+                <div class="col-sm-10">
+                  <input type="text" name="query" placeholder="Rechercher un article ...">
+                </div>
+                <div class="col-sm-2">
+                    <input type="submit" value="OK!">
+                </div>
+            </form>
+        </div>
+        <div class="row">
+          <?php
+          foreach ($articles as $article) :
+          ?>
+            <div class="col-md-4 col-sm-6">
+                <article>
+                    <img src="<?= $article["image"] ?>" alt="<?= $article["image"] ?>">
+                    <p class="date">Posté le <time datetime="<?= $article["publication"] ?>"><?= $article["publication"] ?></time></p>
+                    <h1><?= $article["titre"] ?></h1>
+                    <p><?= $article["accroche"] ?></p>
+                    <a href="article.html">Lire l'article</a>
+                </article>
+            </div>
+          <?php
+          endforeach;
+          ?>
+
+        </div>
+        <footer>
+            <div class="row">
+                <div class="col-xs-12">
+                    <a href="contact.html">Contact</a> - <a href="mentions.html">Mentions légales</a> - <a href="https://www.facebook.com/infoprog.tuto">Facebook</a>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
